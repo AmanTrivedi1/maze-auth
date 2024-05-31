30f9820a95b09ab0cb8c0fea511fbed4d8ac4955
AmanTrivedi1 <amantrivedi598@gmail.com>
2024-05-31 07:10:53 +0530

    removing error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+"use client"
 import Image from 'next/image'
-import Link from 'next/link'
+import axios from "axios";
+import Link from "next/link";
+import React, {useState} from "react";
+import {toast} from "react-hot-toast";
+import {useRouter} from "next/navigation";
+
 
 export default function Home() {
+
+  const router = useRouter()
+  const [data, setData] = useState("nothing")
+  const logout = async () => {
+      try {
+          await axios.get('/api/users/logout')
+          toast.success('Logout successful')
+          router.push('/login')
+      } catch (error:any) {
+          console.log(error.message);
+          toast.error(error.message)
+      }
+  }
+
   return (
     <>
       <div className="h-screen">
@@ -27,6 +47,13 @@ export default function Home() {
             </div>
           </>
         </div>
+        <div>
+             <button
+               onClick={logout}
+               className=" absolute  bg-[#FFDF2B] text-black px-4 py-2 rounded-lg right-4 top-4">
+               Logout
+              </button>
+            </div>
     </>
   )
 }
